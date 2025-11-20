b3dd434bea2d5c113f3a35bf4a6509f89985b29c
Andrea Panattoni <apanatto@redhat.com>
2025-11-20 11:23:04 +0100

    e2e: Capture shutdown logs
    Signed-off-by: Andrea Panattoni <apanatto@redhat.com>

--- e2e/run_all_tests.sh
+++ e2e/run_all_tests.sh
@@ -7,12 +7,19 @@ pushd ${E2E}
 suite_failed=0
 
 for f in ./tests/*.bats; do
+    # Dump logs to a file to catch shutdown errors
+    tmp_logs="$(mktemp -d)"
+    ./bin/kubectl logs -n kube-system ds/multi-networkpolicy-ds-amd64 --since=30s -f --all-pods > "${tmp_logs}/daemon-set.logs" &
+
     bats $f
     retval=$?
     if [ $retval -ne 0 ]; then
         suite_failed=1
         ./bin/kind export logs ./artifacts/`basename $f`.test/kind-logs
+        cp "${tmp_logs}/daemon-set.logs" ./artifacts/`basename $f`.test/daemon-set.logs
     fi
+
+    rm -rf "$tmp_logs"
 done
 
 exit $suite_failed
